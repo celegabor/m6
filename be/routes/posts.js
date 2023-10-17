9534c276e0d152721b0c3ac23e2161d0bf247b8f
Celestin Gabor <127986056+celegabor@users.noreply.github.com>
2023-10-17 18:01:40 +0200

    aggiunta bozza commenti + vari commenti/modifiche

--- be/routes/posts.js
+++ be/routes/posts.js
@@ -1,13 +1,17 @@
+// richiedi le varie cose
 const express = require('express');
 const PostModel = require('../models/post')
+
+// rotta dei post
 const posts = express.Router();
+
 const validatePost = require('../middlewares/validatePost')
 const multer = require('multer')
 const cloudinary = require('cloudinary').v2
 const { CloudinaryStorage } = require('multer-storage-cloudinary')
-require('dotenv').config()
 const crypto = require('crypto');
-const { json } = require('body-parser');
+// importa dati .env (dati nascost)
+require('dotenv').config()
 
 
 cloudinary.config({
@@ -27,10 +31,12 @@ const cloudStorage = new CloudinaryStorage({
 
 const internalStorage = multer.diskStorage({
     destination: (req, file, cb) => {
+        // posizione in cui salvare i file
         cb(null, 'pubblic')
     },
     filename: (req, file, cb) => {
 
+        
         const uniqueSuffix = `${Date.now()}-${crypto.randomUUID()}`
         const fileExtension = file.originalname.split('.').pop()
         cb(null, `${file.fieldname}-${uniqueSuffix}.${fileExtension}`)
@@ -41,6 +47,7 @@ const internalStorage = multer.diskStorage({
 const upload = multer({ storage: internalStorage })
 const cloudUpload = multer({ storage: cloudStorage })
 
+// post su pubblic sul locale
 posts.post('/posts/post/upload', upload.single('cover') ,async ( req, res ) => {
     
     const url = `${req.protocol}://${req.get('host')}`
@@ -48,7 +55,7 @@ posts.post('/posts/post/upload', upload.single('cover') ,async ( req, res ) => {
     try {
         const imgUrl = req.file.filename;
 
-        res.status(200).send({img: `${url}/pubblic/${imgUrl}`, 
+        res.status(200).json({img: `${url}/pubblic/${imgUrl}`, 
             statusCode: 200,
             message: "file caricato con successo",
         })
@@ -61,40 +68,54 @@ posts.post('/posts/post/upload', upload.single('cover') ,async ( req, res ) => {
     }
 })
 
-posts.post('/posts/post/cloudUpload', cloudUpload.single('cover'), async(req, res) => {
+// post su cloudinary
+posts.post('/posts/post/cloudUpload', cloudUpload.single('cover'), async (req, res) => {
     try {
-        res.status(200).json({ cover: req.file.path })
+        const cover = req.file.path; 
+        res.status(200).json({ cover });
     } catch (e) {
         res.status(500).send({
-            statusCode:500,
+            statusCode: 500,
             message: "errore interno del server",
             error: e
-        })
+        });
     }
-})
+});
 
-
+// posts.post('/posts/post/cloudUpload', cloudUpload.single('cover'), async(req, res) => {
+//     try {
+//         res.status(200).json({ cover: req.file.path })
+//     } catch (e) {
+//         res.status(500).send({
+//             statusCode:500,
+//             message: "errore interno del server",
+//             error: e
+//         })
+//     }
+// })
 
 
 
 // get
 posts.get('/posts/get', async (req, res) => {
 
+    // dichiaro la quantità di articoli da vedere x pag, poi gestisco la cosa quando faccio la get gestendo il num della pag. volendo posso modificare la pageSize
     const{ page = 1, pageSize = 10} = req.query
 
     try {
-       const posts = await PostModel.find()
-            .populate('author')
-            .limit(pageSize)
-            .skip((page -1) * pageSize );
 
-       const totalPosts = await PostModel.count();
+       const posts = await PostModel.find()
+            .populate('author')//popola author nell'oggetto
+            .limit(pageSize)//limite preso da sopra
+            .skip((page -1) * pageSize );//dall'ultima pag salta n.(pageSize) articoli
+
+       const totalPosts = await PostModel.count();//metodo che permette di contare quanti post ci sono
 
        res.status(200)
         .send({
             statusCode: 200,
             courentPage: Number(page),
-            totalPages: Math.ceil(totalPosts / pageSize ),
+            totalPages: Math.ceil(totalPosts / pageSize ),//ceil da numeri interi
             totalPosts,
             posts
         })
@@ -106,7 +127,6 @@ posts.get('/posts/get', async (req, res) => {
         })
     }
 });
-
 
 // get by Id
 posts.get('/posts/get/:postId' , async (req, res) => {
@@ -134,6 +154,75 @@ posts.get('/posts/get/:postId' , async (req, res) => {
     }
 });
 
+// get by title
+posts.get('/posts/get/bytitle', async (req, res) => {
+    const { title } = req.query;
+
+    try {
+        const postByTitle = await PostModel.find({
+            // Cerca all'interno del campo 'title' utilizzando regex
+            title: {
+                $regex: new RegExp(title, 'i'), // 'i' indica la corrispondenza senza distinzione tra maiuscole e minuscole
+            }
+        });
+
+        res.status(200).send(postByTitle);
+        
+    } catch (e) {
+        res.status(500).send({
+            statusCode: 500,
+            message: "Errore interno del server",
+            error: e
+        });
+    }
+});
+
+// get by date
+posts.get('/posts/get/:date', async (req, res)=>{
+    const { date } = req.params
+
+    try {
+
+        // farà da aggregatore a tutti i post che rispettano questa condizione
+        const getPostByDate = await PostModel.aggregate([
+            {
+                $match: {
+                    $expr:{
+                        $and:[
+                            {
+                                $eq: [
+                                    { $dayOfMonth: '$createdAt' },
+                                    { $dayOfMonth: new Date(date)}
+                                ]
+                            },
+                            {
+                                $eq: [
+                                    { $month: '$createdAt'},
+                                    { $month: new Date(date)}
+                                ]
+                            },
+                            {
+                                $eq: [
+                                    { $year: '$createdAt'},
+                                    { $year: new Date(date)}
+                                ]
+                            }
+                        ]
+                    }
+                }
+            }
+        ])
+        
+        res.status(200).send(getPostByDate)
+
+    } catch (e) {
+        res.status(500).send({
+            statusCode: 500,
+            message: "Errore interno del server",
+            error: e
+        });
+    }
+})
 
 // post
 posts.post('/posts/post', validatePost, async (req, res)=>{
@@ -172,7 +261,6 @@ posts.post('/posts/post', validatePost, async (req, res)=>{
     }
 });
 
-
 // put
 posts.put('/posts/put/:postId', async (req, res)=>{
     const { postId } = req.params;
@@ -182,7 +270,7 @@ posts.put('/posts/put/:postId', async (req, res)=>{
     if (!postExist){
         return res.status(404).send({
             statusCode: 404,
-            message: "this post does not exist!"
+            message: "This post does not exist!"
         })
     }
 
@@ -222,7 +310,7 @@ posts.delete('/posts/delete/:postId', async (req, res)=>{
 
         res.status(200).send({
             statusCode: 200,
-            message: "post cancellato"
+            message: "post deleted"
         })
     } catch (e) {
         res.status(500).send({
